0b6107c69b3c195d132a427c8ae309d8009ddb53
Daan Dvl <accounts@daandvl.nl>
2020-12-05 14:05:11 +0100

    Tried optimizing, failed horribly

--- src/main/java/nl/daandvl/solutions/year2020/Day05.java
+++ src/main/java/nl/daandvl/solutions/year2020/Day05.java
@@ -19,15 +19,19 @@ public class Day05 extends Solution {
 
     @Override
     protected void run(List<String> input) {
-        for(String seatString : input) {
+        for (String seatString : input) {
             int seatId = new Seat(seatString).getSeatID();
-            if(seatId > a) a = seatId;
+            if (seatId > a) a = seatId;
         }
 
         /* Ugly bruteforce this will take ages */
         for (int row = 0; row < 127; row++) {
             for (int col = 0; col < 8; col++) {
-                if(Seat.isAvailable(row, col)) {
+                int available = Seat.isAvailable(row, col);
+
+                if (available == -1) break;
+
+                if (available == 1) {
                     b = new Seat(row, col).getSeatID();
                     break;
                 }
@@ -37,16 +41,16 @@ public class Day05 extends Solution {
 
     private static class Seat {
         private String seatString;
-        private int row;
-        private int column;
+        private final int row;
+        private final int column;
         private int id = -1;
 
         private static List<Seat> seats = new ArrayList<>();
 
         public Seat(String seatString) {
             this.seatString = seatString;
-            calculateRow();
-            calculateColumn();
+            this.row = calculateRow();
+            this.column = calculateColumn();
             seats.add(this);
         }
 
@@ -57,51 +61,51 @@ public class Day05 extends Solution {
         }
 
         public int getSeatID() {
-            if(this.id == -1) id = row * 8 + column;
+            if (this.id == -1) id = row * 8 + column;
             return id;
         }
 
-        public void calculateRow() {
+        public int calculateRow() {
             int minRow = 0;
             int maxRow = 127;
             for (char c : seatString.substring(0, 7).toCharArray()) {
                 if (c == 'B') {
-                    minRow += (maxRow-minRow)/2+1;
+                    minRow += (maxRow - minRow) / 2 + 1;
 
                 } else if (c == 'F') {
-                    maxRow -= (maxRow-minRow)/2+1;
+                    maxRow -= (maxRow - minRow) / 2 + 1;
                 }
             }
-            this.row = minRow;
+            return minRow;
         }
 
-        public void calculateColumn() {
+        public int calculateColumn() {
             int minCol = 0;
             int maxCol = 7;
             for (char c : seatString.substring(7, 10).toCharArray()) {
                 if (c == 'R') {
-                    minCol += (maxCol-minCol)/2+1;
+                    minCol += (maxCol - minCol) / 2 + 1;
                 } else if (c == 'L') {
-                    maxCol -= (maxCol-minCol)/2+1;
+                    maxCol -= (maxCol - minCol) / 2 + 1;
                 }
             }
-            this.column = minCol;
+            return minCol;
         }
 
-        public static boolean isAvailable(int row, int col) {
+        public static int isAvailable(int row, int col) {
             Seat seat = getSeatByRowAndCol(row, col);
 
             if (seat != null) {
-                return false;
+                return 0;
             }
 
             int seatId = row * 8 + col;
 
             if (getSeatById(seatId + 1) == null || getSeatById(seatId - 1) == null) {
-                return false;
+                return -1;
             }
 
-            return true;
+            return 1;
         }
 
         public static Seat getSeatByRowAndCol(int row, int col) {
